f450a6ad3075a5d8ac68a3c348210c999e72e920
sravan <sravan@qburst.com>
2016-03-14 17:24:42 +0530

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+
+gulp.task('compile', () => {
+	return gulp.src('src/js/**/*.js')
+        .pipe(concat('app.js'))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('default', ['compile']);
